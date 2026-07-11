4585ad9dd43c2661461f876808cd667ffe273c99
jankik04 <jankibk04@gmail.com>
2026-07-11 01:04:16 -0700

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-const reveals = document.querySelectorAll('.reveal');
-const observer = new IntersectionObserver((entries) => {
-  entries.forEach(e => {
-    if (e.isIntersecting) {
-      e.target.classList.add('visible');
-    }
-  });
-}, { threshold: 0.12 });
-reveals.forEach(el => observer.observe(el));
